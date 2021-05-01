6b85474024446291590fbb7bc5776489ce11eac0
Ahmed Sadaqa <afs18@mail.aub.edu>
2021-05-01 18:32:24 +0300

    Change the routing of the login

--- GambStore/GambStore/settings.py
+++ GambStore/GambStore/settings.py
@@ -133,7 +133,7 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'store-home'
+LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
